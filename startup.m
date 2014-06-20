70e0c6e6d6c5ae929e28d84fc08a7137accdb482
Travis Collins <travisfcollins@gmail.com>
2014-06-20 13:18:57 -0400

    Updated startup script
    Now new folders are added automagically at matlab startup

--- startup.m
+++ startup.m
@@ -1,2 +1,11 @@
+% Generate Path for folders
+if exist('pathdef.m', 'file') == 2
+	system('rm pathdef.m');
+end
+system('cp pathdefOrg.m pathdef.m')
+P = genpath(pwd);
+addpath(P);
+
+% Setup SDRU
 setupsdru([pwd,'/sdru']);
 usrp_startup;

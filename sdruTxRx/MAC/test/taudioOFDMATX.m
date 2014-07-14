4ce6ef0ff30bea6bbdeb6d553d80ba85e7b7d5a6
Travis Collins <travisfcollins@gmail.com>
2014-07-14 16:40:39 -0400

    Future MAC test to complete

--- sdruTxRx/MAC/test/taudioOFDMATX.m
+++ sdruTxRx/MAC/test/taudioOFDMATX.m
@@ -7,6 +7,7 @@ filename2 = '';
 AFR1 = dsp.AudioFileReader(filename1,'SamplesPerFrame',SamplesPerFrame);
 AFR2 = dsp.AudioFileReader(filename2,'SamplesPerFrame',SamplesPerFrame);
 
+
 Song1 = [];Song2 = [];
 
 while ~isDone(AFR1) && ~isDone(AFR2) 
@@ -20,6 +21,9 @@ Song1 = [Song1 audio1];
 Song2 = [Song2 audio2];
 
 end
+
+release(AFR1);
+release(AFR2);
 
 Frames = length(Song1)/SamplesPerFrame;
 

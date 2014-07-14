caa52374046a06927ecb392aa3be8093f40283ce
Travis Collins <travisfcollins@gmail.com>
2014-07-14 14:49:20 -0400

    added CPP gen to sdruTxRx folder

---
+++ sdruTxRx/GenLib/genCPPRX.m
@@ -0,0 +1,81 @@
+
+libraryName = 'testRX';
+functionsToThread = {'PHYReceiverFun'};
+cppFilename = 'phyRX';
+compileFolder = 'BUILD';
+LD_LIBRARY_PATH = '/opt/MATLAB/R2013a/runtime/glnxa64/';
+additionalFiles = {'tmwtypes.h','rtwtypes.h'};
+
+
+%MATLAB 2013a LD_LIBRARY_PATH=/opt/MATLAB/R2013a/runtime/glnxa64/
+%MATLAB 2013b LD_LIBRARY_PATH=/usr/local/MATLAB/R2013b/runtime/glnxa64/
+
+%%%%%% DO NOT EDIT BELOW %%%%%%%
+
+% Add library to LD_LIBRARY_PATH
+command = ['export LD_LIBRARY_PATH=',LD_LIBRARY_PATH];
+system(command);
+
+decimation = 20;
+
+codegenCommand = 'codegen -config:dll';
+includeFiles = '-I ../SupportFiles';
+argsCommand='-args {decimation}';
+functionsToThreadStr = sprintf('%s.m ' ,functionsToThread{:});
+
+outputCommand = ['-o ',libraryName];
+
+% Build Codegen Command
+fullCommand = [codegenCommand,' -report ',argsCommand,' ',includeFiles,' ',functionsToThreadStr,' ',outputCommand];
+
+disp('Generating Code');
+eval(fullCommand);
+disp('Completed code generation');
+
+% Make build folder
+mkdir(compileFolder);
+
+% Copy files into build folder
+for file = 1:length(functionsToThread)
+	filename = [functionsToThread{file},'.h'];
+	command = ['cp codegen/dll/','*/',filename,' ',compileFolder];
+	system(command);
+end
+command = ['cp codegen/dll/','*/',libraryName,'.so ',compileFolder];
+system(command);
+
+% Copy Library To Runtime folder
+command = ['sudo cp codegen/dll/','*/',libraryName,'.so',' ',LD_LIBRARY_PATH];
+system(command);
+
+% Copy additional files to build folder
+%for file =1:length(additionalFiles)
+%	command = ['cp codegen/dll/*/',additionalFiles{file},' ',compileFolder];
+%	system(command);
+%end
+command = ['cp codegen/dll/*/* ',compileFolder];
+system(command);
+
+% Copy CPP to build folder
+%command = ['cp ../SupportFiles/',cppFilename,'.cpp ',compileFolder];
+command = ['cp ../SupportFiles/* ',compileFolder];
+system(command);
+
+% Copy USRP required files
+command = ['cp -r /home/sdruser/git/traviscollins/SupportPackage/sdru/include/* ', compileFolder];
+system(command);
+
+% Move to build folder
+cd(compileFolder);
+
+% MBuild command
+command = ['mbuild ',cppFilename,'.cpp ',libraryName,'.so'];
+eval(command);
+
+% Run script
+command = ['./',cppFilename];
+system(command);
+
+
+
+

7254c286b796db099e61490d5c7b18fd09e895c6
Travis Collins <travisfcollins@gmail.com>
2014-07-15 10:36:17 -0400

    adding wrapper for DLL codegen for PHYRx class

---
+++ sdruTxRx/PHY/PHYReceiverFun.m
@@ -0,0 +1,15 @@
+function PHYReceiverFun(num)
+
+fprintf('%d\n',int16(num));
+
+RX = PHYReceiver;
+%RX = PHYReceiver('HWAttached',true);
+%RX.HWAttached = true;
+input = randi(48*4,1);
+
+for k=1:10
+	output = step(RX,input);
+	fprintf('%d\n',int16(length(output)));
+end
+
+end

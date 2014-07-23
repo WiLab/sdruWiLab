1253712f78c50d36b012e5384c901d23ca7fef5e
Travis Collins <travisfcollins@gmail.com>
2014-07-23 09:15:24 -0400

    CPP based code tested and working on windows

--- sdruCPP/Example/MyFiles/CPP/main.cpp
+++ sdruCPP/Example/MyFiles/CPP/main.cpp
@@ -20,16 +20,15 @@
 std::mutex mtx;
 std::queue<std::string> rx2txQueue;
 
-
+//Thread 1
 void Thread_TX(void)
 {
-
     std::cout<<"Started Thread TX"<<std::endl;
     int k = 10;
     while (k>0) {
+        
         mtx.lock();
         if (!rx2txQueue.empty()){
-            
             Function1();
             std::cout<<"Output: "<<rx2txQueue.front()<<std::endl;
             rx2txQueue.pop();
@@ -38,47 +37,38 @@ void Thread_TX(void)
         }
         mtx.unlock();
     }
-    
 }
-
-
-
+//Thread 2
 void Thread_RX(void)
 {
-        std::cout<<"Started Thread RX"<<std::endl;
-	int k;
-	char message[50];
-	for (k=0;k<10;k++){
-
-		Function2();
+    std::cout<<"Started Thread RX"<<std::endl;
+    int k;
+    char message[50];
+    for (k=0;k<10;k++){
         
-        	mtx.lock();
-		sprintf(message,"Message %d",k);
-        	rx2txQueue.push(message);
-        	mtx.unlock();
-	}
-
-    
+        Function2();
+        mtx.lock();
+        sprintf(message,"Message %d",k);
+        rx2txQueue.push(message);
+        mtx.unlock();
+    }
 }
+
 
 int main()
 {
+    //Initialize Matlab Create Functions
     ComboFunction_initialize();
-    char *message;
-    
     
     //Spawn Thread
-    //std::thread thread1( Thread_RX, std::ref(rx2txQueue) );
-    //std::thread thread2( Thread_TX, std::ref(rx2txQueue) );
     std::thread thread1( Thread_RX );
     std::thread thread2( Thread_TX );
-
-    //std::cout<<Function1()<<std::endl;
-    //std::cout<<Function2()<<std::endl;
 
     //Wait for thread to finish
     thread1.join();
     thread2.join();
+    std::cout<<"Threads completed"<<std::endl;
+    
     ComboFunction_terminate();
     
     return 0;

9031172d61f22fd4d578ca61cc0d5a2c6b7f8eb7
Travis Collins <travisfcollins@gmail.com>
2014-09-24 10:48:40 -0400

    separate build for rx made

---
+++ sdruCPP/Example/MyFiles/CPP/mainRx_Split.cpp
@@ -0,0 +1,152 @@
+/*
+ ** main.cpp
+ */
+#include <string>
+#include <iostream>
+#include <thread>
+#include "main.h"
+
+//Include headers of matlab functions
+#include "GetUSRPData.h"
+#include "FindtheFrame.h"
+#include "SignalCorrect.h"
+#include "Decoder.h"
+
+//Include header of combined library
+#include "RX_initialize.h"
+#include "RX_terminate.h"//Not sure if needed
+
+#define MESSAGES2TX 100000//Messages to send between PHY and MAC (NOT USED)
+
+// Create Mutex
+std::mutex mtx;
+std::mutex mtx2;
+std::mutex mtx3;
+
+std::queue<creal_T*> rx2txQueueData;
+std::queue<creal_T*> usrp2FindtheFrameQueue;
+std::queue<boolean_T*> rx2txQueueDataDecode;
+
+// Create Conditional
+std::condition_variable cond;
+std::condition_variable cond2;
+std::condition_variable cond3;
+
+//USRP Thread
+void GetDataUSRP(void)
+{
+    std::cout<<"Started Get USRP Thread"<<std::endl;
+    GetUSRPData_init();
+    GetUSRPData();
+}
+
+// Locator
+void FindTheFrame_Thread(void)
+{
+
+    std::cout<<"Started FindTheFrame Thread"<<std::endl;
+    creal_T *input;
+    creal_T output[1920];
+    short int flag = 1;
+    int k = MESSAGES2TX;
+    while (k>0) {
+        
+        std::unique_lock<std::mutex> locker(mtx3);
+        cond3.wait(locker,[](){ return !usrp2FindtheFrameQueue.empty();});
+
+        input = (usrp2FindtheFrameQueue.front());
+        usrp2FindtheFrameQueue.pop();
+	
+        locker.unlock();
+
+        FindtheFrame(input, output, &flag);
+        if (flag<1){//Frame found
+            std::unique_lock<std::mutex> locker2(mtx);
+            rx2txQueueData.push(&output[0]);
+            locker2.unlock();
+            cond.notify_one(); // Notify waiting thread
+        }
+
+    }
+
+}
+
+
+//Apply CFO and equalizer corrections
+void SignalCorrect_Thread(void)
+{
+    std::cout<<"Started Signal Correct Thread"<<std::endl;
+    creal_T *input;
+    boolean_T output[960];
+    int k = MESSAGES2TX;
+    while (k>0) {
+        
+        std::unique_lock<std::mutex> locker(mtx);
+        cond.wait(locker,[](){ return !rx2txQueueData.empty();});
+        
+        input = (rx2txQueueData.front());
+        rx2txQueueData.pop();
+        
+        locker.unlock();
+        
+        SignalCorrect(input, output);
+        
+        std::unique_lock<std::mutex> locker2(mtx2);
+        rx2txQueueDataDecode.push(&output[0]);
+        locker2.unlock();
+        cond2.notify_one(); // Notify waiting thread
+        
+    }
+}
+
+//Decoder Thread
+void Decoder_Thread(void)
+{
+    std::cout<<"Decoder Thread"<<std::endl;
+    boolean_T *input;
+    int k = MESSAGES2TX;
+    while (k>0) {
+        
+        std::unique_lock<std::mutex> locker(mtx);
+        cond2.wait(locker, [](){ return !rx2txQueueDataDecode.empty();});
+        
+        input = (rx2txQueueDataDecode.front());
+        rx2txQueueDataDecode.pop();
+        locker.unlock();
+        
+        Decoder(input);
+        
+    }
+}
+
+int main()
+{
+    std::cout<<"Main Started"<<std::endl;
+    
+    //Initialize Matlab Create Functions
+    RX_initialize();
+    
+    //Spawn Thread
+    std::thread thread1( GetDataUSRP );
+
+    // Create X threads for task
+    std::vector<std::thread> FindTheFrameThreads;
+    for (int i = 0; i < 1; i++)
+    	FindTheFrameThreads.push_back(std::thread( FindTheFrame_Thread ));
+
+
+    std::thread thread2( SignalCorrect_Thread );
+    std::thread thread3( Decoder_Thread );
+    
+    //Wait for thread to finish
+    thread1.join();
+    thread2.join();
+    thread3.join();
+    for (auto& t : FindTheFrameThreads)
+    	t.join();
+    std::cout<<"Threads completed"<<std::endl;
+    
+    RX_terminate();
+    
+    return 0;
+}
---
+++ sdruCPP/Example/builder_rx.m
@@ -0,0 +1,79 @@
+cfg = coder.config('exe');
+cfg.CustomSource = 'mainRx_Split.cpp';
+cfg.CustomInclude = 'MyFiles/CPP/';
+cfg.TargetLang='C++';
+cfg.PostCodeGenCommand = 'setbuildargs(buildInfo)';
+functionsToThread = {'Transmitter', 'FindtheFrame','GetUSRPData','SignalCorrect','Decoder'};
+additionalSourceFiles = {'add2q.cpp','main.h'};
+outputFunctionName = {'RX'};
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Do Not Edit Below
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Run Codegen, if earlier than 2014, must add include paths manually for
+% sdru
+
+Release = ver;Release = Release.Release;
+Release = str2double(Release(3:6));
+if Release > 2013
+    codegen('-config','cfg',functionsToThread{:},additionalSourceFiles{:},'-o',outputFunctionName{:});
+else
+    hostname = lower(gethostname);
+    hostname = hostname(2:end);
+    setenv('SDRU_DEPLOYMENT_HOSTNAME', hostname);
+    sdruPath = sdruroot;
+    sandboxRoot = fullfile(matlabroot, 'toolbox', 'shared', 'sdr', 'sdru');
+    isSandboxRun = strcmp(sdruPath, sandboxRoot);
+    
+    % Use install paths
+    incPath = fullfile(sdruPath, 'include');
+    libPathPre = sdruPath;
+    
+    % extern/include must come before src/include
+    includeDir = [fullfile(matlabroot, 'extern', 'include') ' ' ...
+        fullfile(matlabroot, 'src', 'include') ' ' incPath];
+    if isempty(cfg.CustomInclude)
+        cfg.CustomInclude = includeDir;
+    else
+        cfg.CustomInclude = [cfg.CustomInclude ' ' includeDir];
+    end
+    
+    arch = computer('arch');
+    switch (arch)
+        case {'glnxa64', 'glnx86'}
+            libpath = fullfile(libPathPre, 'bin', arch);
+            libname = 'libmwusrp_uhd_capi.so';
+        case {'win32','win64'}
+            libpath = fullfile(libPathPre, 'lib', arch);
+            libname = 'libmwusrp_uhd_capi.lib';
+        case {'maci64'}
+            libpath = fullfile(libPathPre, 'bin', arch);
+            libname = 'libmwusrp_uhd_capi.dylib';
+        otherwise
+            error(message('sdru:compilesdru:UnsupportedPlatform'));
+    end
+    if isempty(cfg.CustomLibrary)
+        cfg.CustomLibrary = fullfile(libpath, libname);
+    else
+        cfg.CustomLibrary = [cfg.CustomLibrary ' ' fullfile(libpath, libname)];
+    end
+    
+    
+    if (cfg.InlineThreshold ~= 10) && (cfg.InlineThreshold < 200)
+        warning(message('sdru:compilesdru:InlineThreshold'))
+    end
+    cfg.InlineThreshold = 200;
+    
+    if (cfg.InlineThresholdMax ~= 200) && (cfg.InlineThreshold < 400)
+        warning(message('sdru:compilesdru:InlineThresholdMax'))
+    end
+    cfg.InlineThresholdMax = 400;
+    
+    % Pass the rest of the arguments to codegen
+    codegenResult = codegen(functionsToThread{:},additionalSourceFiles{:},'-config','cfg','-o',outputFunctionName{:});
+    
+end
+
+
+
